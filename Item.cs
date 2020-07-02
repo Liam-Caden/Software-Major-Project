e9f2fb1e53820d7e6bb95e2b14f036cc17192553
Liam-Caden <67636689+Liam-Caden@users.noreply.github.com>
2020-07-03 05:49:26 +1000

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    //Declare variables
+    public Sprite itemSprite;
+    public string itemDescription;
+    public bool isKey;
+    public bool isSword;
+    public bool isMoney;
+    public bool isWand;
+    public bool isMines;
+    public bool isLKey;
+    
+
+}
---
+++ VectorValue.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VectorValue : ScriptableObject, ISerializationCallbackReceiver
+{
+    //creates a vector value
+    [Header("Value running in game")]
+    public Vector2 initialValue;
+    [Header("Value by default when starting")]
+    public Vector2 defaultValue;
+
+    public void OnAfterDeserialize() { initialValue = defaultValue; }
+
+    public void OnBeforeSerialize() { }
+
+}
